db220d261c412506ae0272fef7157379966b9557
Yaloys <yanakyrpushko@gmail.com>
2025-03-17 12:28:16 +0200

    modified singleton

--- src/Main.java
+++ src/Main.java
@@ -11,14 +11,14 @@ public class Main {
     public static void main(String[] args) {
 
         CafeMenu.getInstance().showMenu();
-        System.out.println("----------------------------");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         //french cafe
         CafeFactory cafe = new FrenchCafeFactory();
         CoffeeFactory factory  = cafe.getCoffeeFactory();
         Coffee coffee = factory.createCoffee();
         CoffeeBuilder builder = new CoffeeBuilder().createCoffee(coffee);
         coffee.prepare();
-        System.out.println("----------------------------");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
         Coffee customCoffee = builder.addCoffee().addSyrup("Vanilla").build();
         System.out.println(customCoffee);
 
--- src/singleton/CafeMenu.java
+++ src/singleton/CafeMenu.java
@@ -4,12 +4,27 @@ public class CafeMenu
 {
     private static CafeMenu instance;
     private CafeMenu() {}
-    public static CafeMenu getInstance()
-    {
-        if (instance == null)
-            instance = new CafeMenu();
-        return instance;
+//    public static CafeMenu getInstance()
+//    {
+//        if (instance == null)
+//            instance = new CafeMenu();
+//        return instance;
+//    }
+public static CafeMenu getInstance() {
+    CafeMenu result = instance;
+    if (result == null) {
+        synchronized (CafeMenu.class) //гарант, що об'єкт створюється тільки в одному потоці
+        {
+            result = instance;
+            if (result == null)  //не дозволяє іншому потоку створити новий об'єкт
+            {
+                result = new CafeMenu();
+                instance = result;
+            }
+        }
     }
+    return result;
+}
     public void showMenu()
     {
         System.out.println("Menu: Espresso, Cappuccino, Latte");
